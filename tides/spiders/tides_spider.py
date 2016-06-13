4a2fd2beec55220dabe61b700ff1bac90cfc6748
Trevor Postma <txpost@hotmail.com>
2016-06-13 19:33:42 -0300

    build first level url

--- tides/spiders/tides_spider.py
+++ tides/spiders/tides_spider.py
@@ -7,13 +7,14 @@ class TidesSpider(scrapy.Spider):
     'http://tides.gc.ca/eng/',
   )
 
+  # first level (region): select#mapSelect > option = http://tides.gc.ca/eng/find/region?id=5
   def parse(self, response):
     for region in response.xpath('//option'):
-      value = region.xpath('@value').extract()
-      name = region.xpath('text()').extract()
-      print value, name
+      value = region.xpath('@value').extract()[0]
+      name = region.xpath('text()').extract()[0]
+      url = "http://tides.gc.ca/eng/find/zone?id=%s" % value
+      print value, name, url
 
-# first level (region): select#mapSelect > option = http://tides.gc.ca/eng/find/region?id=5
 # second level (zone): select#mapSelect > option = http://tides.gc.ca/eng/find/zone?id=22
 # third level: select#mapSelect > option = http://tides.gc.ca/eng/station?sid=1485
 # fourth level: table
