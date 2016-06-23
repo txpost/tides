8e1f554056f71411f982578c9a414e14306e9ae3
Trevor Postma <txpost@hotmail.com>
2016-06-22 22:18:06 -0300

    start building the final level parse using a single endpoint

--- tides/spiders/tides_spider.py
+++ tides/spiders/tides_spider.py
@@ -3,31 +3,45 @@ import scrapy
 class TidesSpider(scrapy.Spider):
   name = "tides"
   allowed_domains = ["tides.gc.ca"]
-  start_urls = (
-    'http://tides.gc.ca/eng/',
-  )
+  start_urls = [
+    # 'http://tides.gc.ca/eng/'
+    'http://tides.gc.ca/eng/station?sid=1485'
+  ]
 
-  # first level (region): select#mapSelect > option = http://tides.gc.ca/eng/find/region?id=5
   def parse(self, response):
-    for region in response.xpath('//option'):
-      value = region.xpath('@value').extract()[0]
-      name = region.xpath('text()').extract()[0]
-      url = "http://tides.gc.ca/eng/find/zone?id=%s" % value
-      print value, name, url
-      yield scrapy.Request(url, callback=self.parse_second_level)
+    for table in response.xpath('//div[@class="grid-12 indent-medium"]/div/table'):
+      date = table.xpath('@title').extract()[0][-10:]
+      date = date.replace('-', '')
+      print date
+      for tide in table.xpath('.//tbody/tr/td'):
+        print tide
+  
+  # region
+  # select#mapSelect > option 
+  # http://tides.gc.ca/eng/find/region?id=5
+  # def parse(self, response):
+  #   for region in response.xpath('//option'):
+  #     value = region.xpath('@value').extract()[0]
+  #     name = region.xpath('text()').extract()[0]
+  #     url = "http://tides.gc.ca/eng/find/zone?id=%s" % value
+  #     print value, name, url
+  #     yield scrapy.Request(url, callback=self.parse_second_level)
 
-  # second level (zone): select#mapSelect > option = http://tides.gc.ca/eng/find/zone?id=22
-  def parse_second_level(self, response):
-    for zone in response.xpath('//option'):
-      value = zone.xpath('@value').extract()[0]
-      name = zone.xpath('text()').extract()[0]
-      url = "http://tides.gc.ca/eng/station?sid=%s" % value
-      print value, name, url
-      yield scrapy.Request(url, callback=self.parse_final_level)
+  # zone 
+  # select#mapSelect > option 
+  # http://tides.gc.ca/eng/find/zone?id=22
+  # def parse_second_level(self, response):
+  #   for zone in response.xpath('//option'):
+  #     value = zone.xpath('@value').extract()[0]
+  #     name = zone.xpath('text()').extract()[0]
+  #     url = "http://tides.gc.ca/eng/station?sid=%s" % value
+  #     print value, name, url
+  #     yield scrapy.Request(url, callback=self.parse_final_level)
 
-  def parse_final_level(self, response):
-    for station in response.xpath('//table'):
-      print station
-
-# third level: select#mapSelect > option = http://tides.gc.ca/eng/station?sid=1485
-# fourth level: table
+  # sid
+  # table
+  # http://tides.gc.ca/eng/station?sid=1485
+  # def parse_final_level(self, response):
+  #   for station in response.xpath('//table'):
+  #     title = station.
+  #     print station
