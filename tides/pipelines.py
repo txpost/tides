d3cab2f6be25b7c587bd4e7fe8509c1a82e013a7
Trevor Postma <txpost@hotmail.com>
2016-07-19 00:11:56 -0300

    write to csv using item pipeline

--- tides/pipelines.py
+++ tides/pipelines.py
@@ -4,8 +4,13 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
+import csv
 
 class TidesPipeline(object):
-    def process_item(self, item, spider):
-        return item
+  def __init__(self):
+    self.csvwriter = csv.writer(open('items.csv', 'wb'))
+
+  def process_item(self, item, tides_spider):
+    row = [item['sid'], item['date'], item['time'], item['height_meters'], item['height_feet']]
+    self.csvwriter.writerow(row)
+    return item
--- tides/settings.py
+++ tides/settings.py
@@ -10,10 +10,8 @@
 #     http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
 
 BOT_NAME = 'tides'
-
 SPIDER_MODULES = ['tides.spiders']
 NEWSPIDER_MODULE = 'tides.spiders'
-
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'tides (+http://www.yourdomain.com)'
@@ -64,9 +62,9 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'tides.pipelines.SomePipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'tides.pipelines.TidesPipeline': 300
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
